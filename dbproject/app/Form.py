fc2921e1dfd625b1fdb1eb0868748e3e2446a122
iKeiNagai <thomasvasquez68@gmail.com>
2024-03-28 14:57:40 -0400

    added all fields for each form

--- dbproject/app/Form.py
+++ dbproject/app/Form.py
@@ -4,14 +4,17 @@ from .models import Flower, User, Competition
 class Insertflower(forms.ModelForm):
     class Meta:
         model = Flower
+        fields = "__all__"
 
 class Insertuser(forms.ModelForm):
     class Meta:
         model = User
+        fields = "__all__"
 
 class Insertcompetition(forms.ModelForm):
     class Meta:
         model = Competition
+        fields = "__all__"
         
 class Dosearch(forms.Form):
     Search = forms.CharField()
