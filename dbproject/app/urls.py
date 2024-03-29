20d35f443e1d37eb93a57fd7eb2036dff309c44d
iKeiNagai <thomasvasquez68@gmail.com>
2024-03-29 15:32:49 -0400

    change path names and pattern names

--- dbproject/app/urls.py
+++ dbproject/app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name= "home"), #home url
     path('Organizers/',views.organizers, name= "organizers"), #organizer url
-    path('Search/', views.search, name= "search" ), #search url
-    path('ChangeView/', views.changeview, name= "changeview"), #changeview url
+    path('User/', views.search, name= "user" ), #user url
+    path('Competitions/', views.changeview, name= "competitions"), #competition url
     path('pflowers/', views.pflowers, name= "pflowers") #popular flowers url
 ]
