42d9b1a5805cc38ee014387306c72c52e7f9ec56
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-13 14:53:43 -0400

    fix: search forms labels

--- dbproject/app/filters.py
+++ dbproject/app/filters.py
@@ -1,24 +1,31 @@
 import django_filters
-from django_filters import CharFilter, NumberFilter
+from django_filters import CharFilter, NumberFilter, ChoiceFilter
 from .models import User, Organizers, Competition
 
 class thefilter(django_filters.FilterSet) : #filter querysets
 
     #filter field u_name from user model, if contains input it gets it
-    u_name = CharFilter(field_name= 'u_name', lookup_expr="icontains")
-    
+    u_name = CharFilter(field_name= 'u_name', lookup_expr="icontains", label='Participant Name')
+    u_id = NumberFilter(field_name= 'u_id', label='Participant ID')
     class Meta:
         model = User 
-        fields = '__all__' #fields to filter(for form)
+        fields = ['u_name',
+                  'u_id'] #fields to filter(for form)
 
 class OrganizersFilter(django_filters.FilterSet) :
-    o_id = NumberFilter(field_name='o_id', lookup_expr="exact")
+    o_id = NumberFilter(field_name='o_id', lookup_expr="exact", label='Organizer ID')
+    o_name = CharFilter(field_name='o_name', lookup_expr='icontains', label='Organizer Name')
+    specialty = CharFilter(field_name='specialty', lookup_expr='icontains', label='Specialty')
     class Meta: 
         model = Organizers
         fields = '__all__'
 
 class CompetitionsFilter(django_filters.FilterSet) :
-    c_id = NumberFilter(field_name='c_id')
+    c_id = NumberFilter(field_name='c_id', lookup_expr='exact', label='Competition ID')
+    c_name = CharFilter(field_name='c_name', lookup_expr='icontains', label='Competition Name')
+    c_location = CharFilter(field_name='c_location', lookup_expr='icontains', label='Location')
     class Meta:
         model = Competition
-        fields = '__all__'
+        fields = ['c_id',
+                  'c_name',
+                  'c_location']
