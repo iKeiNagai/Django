291728ca64620baf557fd97661629668b4c1a6e3
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-13 14:06:38 -0400

    organizer form

--- dbproject/app/Form.py
+++ dbproject/app/Form.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Flower, User, Competition
+from .models import Flower, User, Competition, Organizers
 
 #forms connected to models include all fields
 class Insertflower(forms.ModelForm):
@@ -16,7 +16,12 @@ class Insertcompetition(forms.ModelForm):
     class Meta:
         model = Competition
         fields = "__all__"
-        
+
+class InsertOrganizer(forms.ModelForm):
+    class Meta:
+        model = Organizers
+        fields = "__all__"
+
 class randc(forms.Form):
     Name = forms.CharField(label="name")
     PersonNo = forms.IntegerField(label="NNumber")
