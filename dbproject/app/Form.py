6367c15036dd1bd78aacd97d77a8010f8d1c0852
iKeiNagai <thomasvasquez68@gmail.com>
2024-04-13 14:26:10 -0400

    feat: customize forms labels

--- dbproject/app/Form.py
+++ dbproject/app/Form.py
@@ -6,21 +6,42 @@ class Insertflower(forms.ModelForm):
     class Meta:
         model = Flower
         fields = "__all__"
+        labels = {
+            'species' : 'Flower Species',
+            'size' : 'Flower Size',
+            'color' : 'Flower Color',
+            'age' : 'Flower Age'
+        }
 
 class Insertuser(forms.ModelForm):
     class Meta:
         model = User
         fields = "__all__"
+        labels = {
+            'u_name' : 'Name',
+            'u_address' : 'Home Address',
+            'entriesno' : 'Entries No'
+        }
 
 class Insertcompetition(forms.ModelForm):
     class Meta:
         model = Competition
         fields = "__all__"
+        labels = {
+            'c_name' : 'Competition Name',
+            'c_location' : 'Location',
+            'c_date' : 'Date (yyyy/mm/dd time)',
+            'participantsno' : 'Participants No'
+        }
 
 class InsertOrganizer(forms.ModelForm):
     class Meta:
         model = Organizers
         fields = "__all__"
+        labels = {
+            'o_name' : 'Organizer Name',
+            'specialty' : 'Organizer Specialty'
+        }
 
 class randc(forms.Form):
     Name = forms.CharField(label="name")
